d0f7bd91e08f3d8018b0088b374e926d6cfbfecf
K0v7 <contato.felipesilva2020@hotmail.com>
2021-10-05 21:16:09 -0300

    Calculadora
    Java

---
+++ src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Calculadora calc = new Calculadora();
+        calc.start();
+        calc.run();
+        calc.setVisible(true);
+    }
+}
